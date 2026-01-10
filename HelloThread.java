d520d4a77496ea942d54c0b25230ec8f9483b92e
majaliwa-joel <majaliwajoel64@gmail.com>
2026-01-11 00:04:24 +0300

    Add files via upload

---
+++ HelloThread.java
@@ -0,0 +1,11 @@
+public class HelloThread extends Thread {
+
+    public void run() {
+        System.out.println("My first thread!");
+    }
+
+    public static void main(String[] args) {
+        HelloThread t = new HelloThread();
+        t.start(); 
+    }
+}
